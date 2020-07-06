d6da1c8c4466eb48b95d2c6d440fbbf11cae9c5c
llliufei79 <67541827+llliufei79@users.noreply.github.com>
2020-07-06 13:40:17 +0800

    add gps status string to plugin result

--- src/android/GaoDeLocation.java
+++ src/android/GaoDeLocation.java
@@ -282,7 +282,8 @@ public class GaoDeLocation extends CordovaPlugin implements SingleLocaitonDelega
             //楼层
             json.put("floor", location.getFloor());
             //设备当前 GPS 状态
-            json.put("gpsAccuracyStatus", location.getGpsAccuracyStatus());
+            json.put("gpsAccuracyStatus", this.serialLocation.getgetGpsAccuracyStatusString(location.getGpsAccuracyStatus()));
+            json.put("gpsAccuracyStatus
             //定位来源
             json.put("locationType", location.getLocationType());
             //定位信息描述
